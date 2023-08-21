226276604d1955a36f7325bd0abeaa239ed8a4da
emilynievesb <nemily1710@gmail.com>
2023-08-21 16:28:47 -0500

    feat: :sparkles: Consultas acudiente
    Se realizaron consultas necesarias para la colección acudiente

---
+++ src/collections/attendant.js
@@ -0,0 +1,47 @@
+import autoIncrementID from "../utils/autoIncrement.js";
+import connection from "../utils/connect.js";
+
+class Attendant {
+  acu_codigo;
+  acu_nombreCompleto;
+  acu_telefono;
+  acu_direccion;
+  constructor() {}
+  async connect() {
+    try {
+      const result = await connection("acudiente");
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async postAttendant() {
+    try {
+      const connection = await this.connect();
+      const resultado = await connection.insertOne({
+        acu_codigo: this.acu_codigo,
+        acu_nombreCompleto: this.acu_nombreCompleto,
+        acu_telefono: this.acu_telefono,
+        acu_direccion: this.acu_direccion,
+      });
+      return resultado;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async searchAttendant() {
+    try {
+      const connection = await this.connect();
+      const resultado = await connection
+        .findOne({
+          acu_codigo: this.acu_codigo,
+        })
+        .toArray();
+      return resultado;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export { Attendant };
